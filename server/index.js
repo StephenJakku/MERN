1e116f5444fc5a6676c5f2aa8311e8ac8298741a
StephenJakku <uttamjakku96@gmail.com>
2022-12-29 16:16:22 -0800

    Basic configuration and filestorage setup done. Mongoose seteup done. server running and listening to port 3001.

--- server/index.js
+++ server/index.js
@@ -9,28 +9,39 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 
-/*CONFIGURATIONS*/
-const __filename=fileURLToPath(import.meta.url);
-const __dirname=path.dirname(__filename);
+// CONFIGURATIONS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(bodyParser.json({limit:"30mb",extended:true}));
-app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/assets",express.static(path.join(__dirname,'public/assets')));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 // FILE STORAGE
 const storage = multer.diskStorage({
-    destination:function(req,file,cb){
-        cb(null,"public/assets")
-    },
-    filename: function(req,file,cb){
-        cb(null,file.originalname)
-    }
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
 });
-const upload=multer({storage});
+const upload = multer({ storage });
 
+// MONGOOSE SETUP
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
